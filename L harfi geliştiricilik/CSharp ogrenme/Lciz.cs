daa902421f0064fe3a03cd98accbad66125cf59e
GeneralX-Z01 <Fuad.Garibli@alu.uclm.es>
2022-11-30 20:14:13 +0300

    L Harfi C#ta hareket ettirilir
    bu kod sayesinde klavyeden alinan inputlarla goruntuyu hareketettirme becerisi kazandim

---
+++ L harfi geliştiricilik/CSharp ogrenme/Lciz.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_ogrenme
+{
+    class Lciz
+    {
+        private int Lyukseklik;
+        private int Lgenislik;
+        public int X;
+        public int Y;
+
+        public Lciz ()
+        {
+            Lyukseklik = 9;
+            Lgenislik = 7;
+            X = 40;
+            Y = 10;
+        }
+        public Lciz(int gen, int yuk)
+        {
+            Lyukseklik = yuk;
+            Lgenislik = gen;
+        }
+        public void goster()
+        {
+            if (X < 0 || X > 112 || Y < 0 || Y>29)
+            {
+                X = 10;
+                Y = 7;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                for (int i = 0; i <= Lyukseklik; i++)
+                {
+                    Console.SetCursorPosition(X, Y + i);
+                    Console.WriteLine(" ");
+                    int z = Y + i;
+
+                    if (i == Lyukseklik)
+                    {
+                        for (int a = 0; a <= Lgenislik; a++)
+                        {
+                            Console.SetCursorPosition(X + a, z);
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+}
+
---
+++ L harfi geliştiricilik/CSharp ogrenme/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CSharp_ogrenme
+{
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lciz harf = new Lciz();
+            while (true)
+            {
+                
+                    ConsoleKeyInfo tus = Console.ReadKey();
+                    if (tus.Key == ConsoleKey.A || tus.Key == ConsoleKey.LeftArrow)
+                    {
+                        harf.X = harf.X - 5;
+                    }
+                    if (tus.Key == ConsoleKey.D || tus.Key == ConsoleKey.RightArrow)
+                    {
+                        harf.X = harf.X + 5;
+                    }
+                    if (tus.Key == ConsoleKey.S || tus.Key == ConsoleKey.DownArrow)
+                    {
+                        harf.Y = harf.Y + 2;
+                    }
+                    if (tus.Key == ConsoleKey.W || tus.Key == ConsoleKey.UpArrow)
+                    {
+                        harf.Y = harf.Y - 2;
+                    }
+                    if (tus.Key == ConsoleKey.Escape)
+                    {
+                        break;
+                    }
+
+                    Console.Clear();
+                    harf.goster();
+                
+            
+            }
+            Console.ReadKey();
+        }
+    }
+}
